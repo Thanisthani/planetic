0f4ce310691ddd2e36b07d2aab711667df7001d5
Thanistas <thanithanis171198@gmail.com>
2022-01-10 10:17:05 +0530

    Add SignUp Screen

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import SignInScreen from './Screen/SignInScreen';
+import SignUpScreen from './Screen/SignUpScreen';
 import WelcomeScreen from './Screen/WelcomeScreen';
 
 export default function App() {
   return (
     // <WelcomeScreen />
-    <SignInScreen />
+    // <SignInScreen />
+    <SignUpScreen />
   );
 }
 
--- Screen/SignUpScreen.js
+++ Screen/SignUpScreen.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, StyleSheet, StatusBar } from 'react-native'
+import SignUp from '../component/signUp/SignUp'
 
 const SignUpScreen = () => {
     return (
-        <View>
-            <Text></Text>
+        <View style={Styles.conatiner}>
+           <SignUp />
         </View>
     )
 }
 
+const Styles = StyleSheet.create({
+    conatiner: {
+        flex: 1,
+        backgroundColor: "white",
+        paddingTop:StatusBar.currentHeight,
+    }
+})
 export default SignUpScreen
